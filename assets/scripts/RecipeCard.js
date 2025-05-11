2f96a15c118d0b7aae1d94f38d04ddb19c1e58b7
Aldrin Ilagan <ailagan@ucsd.edu>
2025-05-10 22:19:13 -0700

    Finished RecipeCard.js

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,13 +8,13 @@ class RecipeCard extends HTMLElement {
 
 		// EXPOSE - START (All expose numbers start with A)
 		// A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
-		const shadowEl = this.attachShadow({mode: 'open'});
+		this.shadow = this.attachShadow({mode: 'open'});
 		// A2. TODO - Create an <article> element - This will hold our markup once our data is set
-		const articleEl = document.createElement('article');
+		const article = document.createElement('article');
 		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-		const styleEl = document.createElement('style')
+		const style = document.createElement('style')
 		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
-		styleEl = 
+		style.textContent = 
 		` * {
 			font-family: sans-serif;
 			margin: 0;
@@ -91,8 +91,8 @@ class RecipeCard extends HTMLElement {
 			font-size: 12px;
 		  }`;
 		// A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-		shadowEl.append(articleEl);
-		shadowEl.append(styleEl);
+		shadow.append(article);
+		shadow.append(style);
 	}
 
 	/**
@@ -121,7 +121,7 @@ class RecipeCard extends HTMLElement {
 		if (!data) return;
 
 		// A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-		article = this.shadowEl.articleEl;
+		selectedArticle = this.shadow.querySelector('article');
 		// A7. TODO - Set the contents of the <article> with the <article> template given in
 		//           cardTemplate.html and the data passed in (You should only have one <article>,
 		//           do not nest an <article> inside another <article>). You should use template
@@ -129,13 +129,23 @@ class RecipeCard extends HTMLElement {
 		// 			 Do NOT include the <article> tags within the innerHTML of the element you create.
 		//           Remember to replace all the placeholders in the template with the data passed in.
 		//           i.e. imgSrc, titleLnk, etc
-		firstImg = document.querySelector('img');
-		firstImg.src = data['imgSrc'];
-		firstImg.alt = data['imgAlt'];
+		thumbnail = selectedArticle.querySelector('img');
+		thumbnail.src = data.imgSrc;
+		thumbnail.alt = data.imgAlt;
+
+		recipeTitle = selectedArticle.getElementsByClass('title')[0];
+		recipeTitle.a.href = data.titleLnk;
+		recipeTitle.a.textContent = data.titleTxt;
 		
+		selectedArticle.getElementsByClass('organization')[0].textContent = data.organization;
 		
+		selectedArticle.querySelector('.rating span').textContent = data.rating;
+		selectedArticle.querySelector('.rating span + span').textContent = data.numRatings;
+		selectedArticle.querySelector('time').textContent = data.lengthTime;
+		selectedarticle.querySelector('.ingredients').textContent = data.ingredients;
 	}
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+customElements.define('recipe-card', RecipeCard);
