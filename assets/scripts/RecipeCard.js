06f8b2802077d2b33d2c6a64fe33146ab2636d6d
Aldrin Ilagan <ailagan@ucsd.edu>
2025-05-11 02:29:31 -0700

    Finished explore

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -10,9 +10,9 @@ class RecipeCard extends HTMLElement {
 		// A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
 		this.shadow = this.attachShadow({mode: 'open'});
 		// A2. TODO - Create an <article> element - This will hold our markup once our data is set
-		const article = document.createElement('article');
+		let article = document.createElement('article');
 		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
-		const style = document.createElement('style')
+		let style = document.createElement('style')
 		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
 		style.textContent = 
 		` * {
@@ -91,8 +91,7 @@ class RecipeCard extends HTMLElement {
 			font-size: 12px;
 		  }`;
 		// A5. TODO - Append the <style> and <article> elements to the Shadow DOM
-		this.shadow.append(article);
-		this.shadow.append(style);
+		this.shadow.append(style, article);
 	}
 
 	/**
@@ -130,10 +129,10 @@ class RecipeCard extends HTMLElement {
 		//           Remember to replace all the placeholders in the template with the data passed in.
 		//           i.e. imgSrc, titleLnk, etc
 		selectedArticle.innerHTML = 
-		`<img src=${data.imgSrc}
-			alt=${data.imgAlt}>
+		`<img src="${data.imgSrc}"
+			alt="${data.imgAlt}">
 		<p class="title">
-			<a href=${data.titleLnk}>${data.titleTxt}</a>
+			<a href="${data.titleLnk}">${data.titleTxt}</a>
 		</p>
 		<p class="organization">${data.organization}</p>
 		<div class="rating">
@@ -151,3 +150,4 @@ class RecipeCard extends HTMLElement {
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
 customElements.define('recipe-card', RecipeCard);
+export default RecipeCard;
